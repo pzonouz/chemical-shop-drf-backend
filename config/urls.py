a1f9f3b06b26c60a884d2410bcbfbc1cfd25e034
Peyman khalili <p.zonouz@gmail.com>
2024-03-11 00:06:07 +0330

    implementing auth by djoser

--- config/urls.py
+++ config/urls.py
@@ -18,4 +18,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-urlpatterns = [path("admin/", admin.site.urls), path("api/", include("api.urls"))]
+urlpatterns = [path("admin/", admin.site.urls), path("api/", include("api.urls")),
+               path(r'api/auth/', include('djoser.urls')),
+               path(r'api/auth/', include('djoser.urls.jwt')),]
